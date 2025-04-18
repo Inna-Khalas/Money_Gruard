8e3ad822364127f7befa07cf93e9ffab779c9c33
Inka <innamakarec349@gmail.com>
2025-04-18 19:47:32 +0400

    fix: swagger

--- src/utils/getEnvVar.js
+++ src/utils/getEnvVar.js
@@ -4,10 +4,8 @@ dotenv.config();
 
 export function getEnvVar(name, defaultValue) {
   const value = process.env[name];
-  console.log(value);
 
   if (value) return value;
-  console.log(value);
 
   if (defaultValue) return defaultValue;
   throw new Error(`Missing: process.env['${name}'].`);
