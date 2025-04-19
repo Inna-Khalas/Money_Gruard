d0b6647d8b523fa8eccc7b529c5a00817e84f78d
O. HADALIN <dyblikat777@gmail.com>
2025-04-19 18:16:24 +0300

    move user registration logic to service

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -1,22 +1,10 @@
-import createHttpError from 'http-errors';
-import bcrypt from 'bcrypt';
-import { UsersCollections } from '../../db/models/user.js';
+import { registerUser } from '../../services/auth.js';
 
 export const register = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
 
-    const existingUser = await UsersCollections.findOne({ email });
-    if (existingUser) {
-      throw createHttpError(409, 'Email in use');
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await UsersCollections.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
+    const newUser = await registerUser(name, email, password);
 
     res.status(201).json({
       status: 201,
