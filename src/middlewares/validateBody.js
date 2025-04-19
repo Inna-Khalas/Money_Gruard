01c4a9ae52c27734a737ec4684cbdb4f50080014
O. HADALIN <dyblikat777@gmail.com>
2025-04-19 15:28:07 +0300

    POST /register

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,10 +1,14 @@
+import createHttpError from 'http-errors';
+
 export const validateBody = (schema) => {
   return (req, _res, next) => {
-    const { error } = schema.validate(req.body);
+    const { error } = schema.validate(req.body, { abortEarly: false });
     if (error) {
-      error.status = 400;
-      return next(error);
+      const message = error.details.map((detail) => detail.message).join(', ');
+      return next(createHttpError(400, message));
     }
     next();
   };
 };
+
+export default validateBody;
