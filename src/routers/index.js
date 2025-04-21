c09bec58068bdff392b88208ab657799c379bf27
Inka <innamakarec349@gmail.com>
2025-04-21 23:09:02 +0400

    fix

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import transactionsRouter from '../routers/transactions.js';
 import authRouter from '../routers/auth.js';
+import { swaggerDocs } from '../middlewares/swaggerDocs.js';
 
 const router = Router();
 
 router.use('/transactions', transactionsRouter);
 router.use('/auth', authRouter);
+router.use('/api-docs', swaggerDocs());
 
 export default router;
