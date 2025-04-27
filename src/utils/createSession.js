56e956270ebd91ea2c978d75da066149a93da354
Andrey Ruts <ravnb1989@gmail.com>
2025-04-27 18:20:21 +0300

    ref token fix

--- src/utils/createSession.js
+++ src/utils/createSession.js
@@ -1,9 +1,10 @@
-import { randomBytes } from 'crypto';
+// import { randomBytes } from 'crypto';
+import crypto from 'node:crypto';
 
 export const createSession = () => {
   return {
-    accessToken: randomBytes(30).toString('base64'),
-    refreshToken: randomBytes(30).toString('base64'),
+    accessToken: crypto.randomBytes(30).toString('base64'),
+    refreshToken: crypto.randomBytes(30).toString('base64'),
     accessTokenValidUntil: new Date(Date.now() + 15 * 60 * 1000),
     refreshTokenValidUntil: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
   };
